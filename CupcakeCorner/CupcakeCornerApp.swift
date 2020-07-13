35e5ee99c6150b2e4f512f656d0e9acb79eeda19
Chris Eadie <chris.eadie@outlook.com>
2020-07-13 14:26:12 +0100

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Chris Eadie on 13/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
