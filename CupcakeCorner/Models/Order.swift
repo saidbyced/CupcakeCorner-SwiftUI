ecabd95de4786af4d3faa59ad7a55cca00760db5
Chris Eadie <chris.eadie@outlook.com>
2020-07-13 16:15:19 +0100

    Add calculated order cost

--- CupcakeCorner/Models/Order.swift
+++ CupcakeCorner/Models/Order.swift
@@ -38,4 +38,37 @@ class Order: ObservableObject {
   @Published var city = ""
   @Published var postcode = ""
   
+  var addressNotValid: Bool {
+    name.isEmpty || streetAddress.isEmpty || city.isEmpty || postcode.isEmpty
+  }
+  
+  // MARK: Price details
+  var cost: Double {
+    // £ per cupcake
+    let cupcakeCost = Double(quantity * 2)
+    
+    // type increases price
+    let typeCost = (Double(type) / 2)
+    
+    // £1/cupcake for extra frosting
+    var extraFrostingCost: Double {
+      if extraFrosting {
+        return Double(quantity)
+      } else {
+        return 0.0
+      }
+    }
+    
+    // £0.50/cupcake for extra sprinkles
+    var extraSprinklesCost: Double {
+      if addSprinkles {
+        return (Double(quantity) / 2)
+      } else {
+        return 0.0
+      }
+    }
+    
+    return cupcakeCost + typeCost + extraFrostingCost + extraSprinklesCost
+  }
+  
 }
